9832133f492f95e8e0151570298529c598ffa0e0
EmptyWu <empty0921.wu@gmail.com>
2021-12-18 18:09:12 +0800

    nodejs lineNotify 首次簽入

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const linenotify=require('./tools/lineNotify');
+const bodyParser = require('body-parser')
+const app=express();
+//app.use(bodyParser.text({type: '*/*'}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.post('/message/',(req,res)=>{
+    var line_message=req.body.line.msg;
+    //console.log('line_message '+line_message);
+    linenotify.line(line_message);
+    res.send('line_message is '+line_message);
+});
+
+var server=app.listen(8080,function(){
+    var host=server.address().address;
+    var port=server.address().port;
+
+    console.log('应用实例，访问地址为 http://%s:%s', host, port);
+});
---
+++ tools/lineNotify.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+var FormData = require('form-data');
+//require('dotenv').config();
+
+function lineNotify (msg ) {
+    const token = '10w4oEZmS0fcPkgid1PgmpK1nKayWMMaxdLI5Z0NMGv';//process.env.LINE_TOKEN;
+    //'10w4oEZmS0fcPkgid1PgmpK1nKayWMMaxdLI5Z0NMGv';
+    // 使用 form-data 傳遞資料
+    const form_data = new FormData();
+    form_data.append("message", msg);
+
+    // 設定 LINE Notify 的 權杖 & form-data
+    const headers = Object.assign({
+        'Authorization': `Bearer ${token}`
+    }, form_data.getHeaders());
+    
+    axios({
+        method: 'post',
+        url: 'https://notify-api.line.me/api/notify',
+        data: form_data,
+        headers: headers
+    }).then(function (response) {
+        // HTTP狀態碼 200 代表成功
+        console.log("HTTP狀態碼:" + response.status);
+        // 觀察回傳的資料是否與 POSTMAN 測試一致
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error("LINE通知發送失敗");
+        if (error.response) { // 顯示錯誤原因            
+            console.error("HTTP狀態碼:" + error.response.status);
+            console.error(error.response.data);
+        } else {
+            console.error(error);
+        }
+    });
+}
+
+//lineNotify('qwe');
+
+module.exports={
+    line:function(msg){
+        lineNotify(msg);
+    }
+    };
