68b781a2e149b89d9235b7019dd559f02004ac12
EmptyWu <empty0921.wu@gmail.com>
2021-12-18 19:06:58 +0800

    增加靜態頁面

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express=require('express');
 const linenotify=require('./tools/lineNotify');
 const bodyParser = require('body-parser')
 const app=express();
+app.use(express.static("public"))
 //app.use(bodyParser.text({type: '*/*'}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>");
+  });
+  
 app.post('/message/',(req,res)=>{
     var line_message=req.body.line.msg;
     //console.log('line_message '+line_message);
@@ -13,9 +18,5 @@ app.post('/message/',(req,res)=>{
     res.send('line_message is '+line_message);
 });
 
-var server=app.listen(80,function(){
-    var host=server.address().address;
-    var port=server.address().port;
-
-    console.log('http://%s:%s', host, port);
-});
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
