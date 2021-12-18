1ba0c40871e8fb1fcb38f20b2a3eb5eaad31033c
EmptyWu <empty0921.wu@gmail.com>
2021-12-18 18:16:25 +0800

    測試變更

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ var server=app.listen(8080,function(){
     var host=server.address().address;
     var port=server.address().port;
 
-    console.log('应用实例，访问地址为 http://%s:%s', host, port);
+    console.log('http://%s:%s', host, port);
 });
